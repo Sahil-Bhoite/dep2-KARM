f6f7d629caec8b5cfd5eee132a9558007589ee68
Sahil Bhoite <99117260+Sahil-Bhoite@users.noreply.github.com>
2025-02-11 13:30:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-GOOGLE_API_KEY = 'AIzaSyA02NGYXBUx66PlOdM4DJvgMcAEa9Sv4FI'
+GOOGLE_API_KEY = 'AIzaSyD67dvcWo-RBQ5lla03eIl2qGuHf74uq-k' # Replace with your actual Google API key
+TOGETHER_API_KEY = '09018cc36965afd53da91adb6117f2084bb34f80f209d7d357bbad8c08bc8b26' # Replace with your actual Together API key
+HUGGING_FACE_TOKEN = "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"
+GROQ_API_KEY = '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'
